d71471689da0ca92819d4ff210e651b5f1120d31
Jack Ryan <jack.dixon.ryan@gmail.com>
2021-04-07 08:37:27 -0600

    Initial commit.

---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>Hi! My name is Jack Ryan, and I built this site! I love space exploration and I love the future of the web. Hopefully this winds up serving as an excellent paean to both. You can <a href="https://github.com/jackdixonryan">check out my Github profile to see some of my other work.</a></p>
